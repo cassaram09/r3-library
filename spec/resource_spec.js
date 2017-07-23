830751e2d91a61abbee30f85d8f61c3164933366
cassaram09 <cassaram09@gmail.com>
2017-07-23 16:59:21 -0400

    add basic test coverage

--- spec/resource_spec.js
+++ spec/resource_spec.js
@@ -1,15 +1,55 @@
 const r3 = require("../lib/resource");
+const Promise = require('promise')
 
 const Resource = r3.default;
 
 describe('Resource class', function () {
-    it('creates a new resource', function () {
-        var headers = {'CONTENT': 'JSON'}
-        var res = new Resource('resource', '/resources', headers, []);
-      
-        expect(res.name).toEqual('resource');
-        expect(res.url).toEqual('/resources');
-        expect(res.headers).toEqual(headers);
-        expect(res.state).toEqual([]);
-    });
+
+  it('creates a new resource', function () {
+      const headers = {'CONTENT': 'JSON'}
+      const res = new Resource('resource', '/resources', headers, []);
+    
+      expect(res.name).toEqual('resource');
+      expect(res.url).toEqual('/resources');
+      expect(res.headers).toEqual(headers);
+      expect(res.state).toEqual([]);
+  });
+
+  it('registers default functions when called', function () {
+      const defaults = new Resource('defaults', '/resources', {}, []).registerDefaults();
+      const noDefaults = new Resource('noDefaults', '/resources', {}, [])
+
+      defaultKeys = Object.keys(defaults.resourceActions)
+      expect(defaultKeys.length).toEqual(5);
+
+      noDefaultKeys = Object.keys(noDefaults.resourceActions)
+      expect(noDefaultKeys.length).toEqual(0);
+  });
+
+  it('successfully registers new actions', function () {
+      const res = new Resource('resource', '/resources');
+
+      res.registerNewAction('/current-user', 'getCurrentUser', 'GET', (state, action) => {return action.data})
+
+      var key = Object.keys(res.resourceActions)[0]
+      expect(res.resourceActions[key]).toBeTruthy()
+      expect(key).toEqual('resource_getCurrentUser')
+  });
+
+  // it('successfully dispatches actions', function () {
+  //     const res = new Resource('resource', '/resources')
+
+  //     res.addReducerAction('test', (state, action) => {return action.data})
+  //     res.resourceActions.resource_test = () => {
+  //       var promise = new Promise((resolve, reject) => {
+  //         true ? resolve({data: 'myData'}) : reject(Error("Error"));
+  //       });
+  //       return promise; 
+  //     }
+
+   
+  //     expect(data).toEqual('myData')
+  // });
+
 });
+
