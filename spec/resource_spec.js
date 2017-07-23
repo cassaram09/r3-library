f0beb31cb25053cf23d08ec67235acea1230380a
cassaram09 <cassaram09@gmail.com>
2017-07-23 15:35:18 -0400

    install jasmine test suite

---
+++ spec/resource_spec.js
@@ -0,0 +1,8 @@
+const Resource = require('../src/resource')
+
+describe('Resource class', function () {
+    it('creates a new resource', function () {
+        var resource = new Resource('resource', '/resources')
+        expect(resource.name).toEqual('resource');
+    });
+});
