8761a773cf374b3e69332e032a54071bebb59b80
cassaram09 <cassaram09@gmail.com>
2017-07-23 16:28:19 -0400

    start writing jasmine tests

--- spec/resource_spec.js
+++ spec/resource_spec.js
@@ -1,6 +1,15 @@
+const r3 = require("../lib/resource");
+
+const Resource = r3.default;
+
 describe('Resource class', function () {
     it('creates a new resource', function () {
-        var resource = new Resource('resource', '/resources')
-        expect(resource.name).toEqual('resource');
+        var headers = {'CONTENT': 'JSON'}
+        var res = new Resource('resource', '/resources', headers, []);
+      
+        expect(res.name).toEqual('resource');
+        expect(res.url).toEqual('/resources');
+        expect(res.headers).toEqual(headers);
+        expect(res.state).toEqual([]);
     });
 });
